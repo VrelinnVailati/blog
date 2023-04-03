12b56a4c6f0b1ebbb560b93c55fa01cc61ab3b9d
Vi Vailati <ambarlife845@gmail.com>
2023-04-03 16:33:28 -0600

    Add PostDetail component

---
+++ src/components/BlogPosts/PostDetail.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Post } from "../../types/Post";
+
+type PostDetailProps = {
+  post: Post;
+};
+
+const { post } = defineProps<PostDetailProps>();
+</script>
+
+<template>
+  <div class="border-t rounded-lg border-lion">
+    <p class="text-lg mx-2.5">{{ post.body }}</p>
+  </div>
+</template>
