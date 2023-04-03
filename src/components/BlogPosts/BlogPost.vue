1e557260a01277d47646ca8c0bdda7b96c497030
Vi Vailati <ambarlife845@gmail.com>
2023-04-03 15:28:20 -0600

    Add vue components for the Blog Posts list

---
+++ src/components/BlogPosts/BlogPost.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { Post } from "../../types/Post";
+
+type BlogPostProps = {
+  posts: Post[];
+};
+
+const { posts } = defineProps<BlogPostProps>();
+</script>
+
+<template>
+  <div
+    class="flex flex-col shadow shadow-dark-purple mb-2"
+    v-for="post in posts"
+    :key="post.id"
+  >
+    <a
+      :href="`blog/${post.id}`"
+      class="hover:bg-light-pink/75 hover:text-black ease-in duration-100"
+    >
+      <div class="ml-2">
+        <div class="flex flex-row items-end">
+          <div class="text-sm mr-2 font-big-title text-lion">
+            DRM-{{ post.id.toString().padStart(3, "0") }}
+          </div>
+
+          <div class="text-2xl font-big-title">{{ post.title }}</div>
+        </div>
+
+        <div class="ml-3">
+          <p class="text-md">{{ post.description }}</p>
+        </div>
+      </div>
+    </a>
+  </div>
+</template>
---
+++ src/components/BlogPosts/BlogPostsList.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import BlogPost from "./BlogPost.vue";
+
+const { posts } = await fetch("http://localhost:3000/api/posts").then((res) =>
+  res.json()
+);
+</script>
+
+<template>
+  <ul>
+    <BlogPost :posts="posts" />
+  </ul>
+</template>
