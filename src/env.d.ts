fca29798d9583f6cceb7e2fc634dda9fb842cabb
Vi Vailati <ambarlife845@gmail.com>
2023-04-03 20:35:15 -0600

    Add typing for env intellisense

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+type ImportMetaEnv = {
+  readonly DATABASE_URL: string;
+  readonly API_URL: string;
+};
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
